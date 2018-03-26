a4a488b03f3debdf0cb9eaee327795b55a6ddd96
SEIF <seif.kowatli@hotmail.com>
2018-03-27 01:22:47 +0200

    All is done
    The counter is done and the  other page component is modified

---
+++ Examination Page/js/script.js
@@ -0,0 +1,60 @@
+
+
+// Set the date we're counting down to
+var countDownDate = new Date(Date.now());
+countDownDate.setHours(countDownDate.getHours() + 2);
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="cd"
+    document.getElementById("cd").innerHTML =  hours + "h "
+    + minutes + "m " + seconds + "s ";
+    
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("cd").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+
+
+
+var countDownDate_q = new Date(Date.now());
+countDownDate_q.setMinutes(countDownDate_q.getMinutes() + 15);
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now an the count down date
+    var distance = countDownDate_q - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="cdq"
+    document.getElementById("cdq").innerHTML =
+    + minutes + "m " + seconds + "s ";
+    
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("cdq").innerHTML = "EXPIRED";
+    }
+}, 1000);
