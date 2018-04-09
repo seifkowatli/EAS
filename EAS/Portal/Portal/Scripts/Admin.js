682b2cbdd4c48023b1209b63e81e4ce753c170e7
Salim-7 <salim200904@gmail.com>
2018-04-09 21:57:28 +0300

    Add User join with API server , we can Add new user and you can show the generated password in pages consol

---
+++ EAS/Portal/Portal/Scripts/Admin.js
@@ -0,0 +1,93 @@
+﻿// Add New User (Register)
+
+
+
+function Close_Alert_Div() {
+
+    $('#successText').hide();
+    $('#errortxt').hide();
+}
+
+function randomPassword(length) {
+    var chars = "abcdefghijklmnopqrstuvwxyz1234567890";
+    var chars2 = "!@#$%^&*()-+<>";
+    var chars3 = "ABCDEFGHIJKLMNOP"
+    var number = "1234567890";
+    var pass = "";
+    for (var x = 0; x < length; x++) {
+        var i = Math.floor(Math.random() * chars.length);
+        pass += chars.charAt(i);
+        var i = Math.floor(Math.random() * chars2.length);
+        pass += chars2.charAt(i);
+        var i = Math.floor(Math.random() * chars3.length);
+        pass += chars3.charAt(i);
+        var i = Math.floor(Math.random() * number.length);
+        pass += number.charAt(i);
+
+    }
+    return pass;
+}
+
+
+$(document).ready(function () {
+
+    $('#successText').hide();
+    $('#errortxt').hide();
+
+    $('#btnRegister').click(function () {
+
+
+        var Random_Password = randomPassword(4);
+        console.log(Random_Password);
+       
+
+   
+        $.ajax({
+            url: 'http://localhost:2199/api/account/register',
+            method: 'POST',
+            headers: { 'Authorization': 'Bearer ' + sessionStorage.getItem('accessToken') },
+            data: {
+                UserName: $('#TextUserName').val(),
+                Phone: $('#TextPhone').val(),
+                Email: $('#TextEmail').val(),
+                UserRole: $('#TextRole').find(":selected").text(),
+                password: Random_Password,
+                confirmpassWord: Random_Password,
+                F_Name: $('#F_Name').val(),
+                L_Name: $('#L_Name').val(),
+
+            },
+            success: function () {
+
+                $('#successText').show('fade');
+
+
+            },
+            
+            error: function (jqXHR) {
+                $('#P_error').text(jqXHR.responseText);
+                $('#errortxt').show('fade');
+            }
+
+
+
+        });
+
+    });
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+//
